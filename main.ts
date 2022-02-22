2f0bc34668551ee154963be4ad340cd9caeb48b9
aioi0125 <99718820+aioi0125@users.noreply.github.com>
2022-02-22 15:20:54 +0900

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
-basic.forever(function () {
+basic.forever(function on_forever() {
     if (input.lightLevel() < 100) {
         basic.showLeds(`
             # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        `)
     } else {
         basic.clearScreen()
     }
+    
 })
